965d2df1f538216cd29cb7362ec20beea33ef020
devang47 <72780418+Devang47@users.noreply.github.com>
2024-01-18 20:44:10 +0530

    feat: view all projects url added

--- src/app/sections/projects/index.tsx
+++ src/app/sections/projects/index.tsx
@@ -23,6 +23,32 @@ const Projects = forwardRef(function Projects(
         <ul className="group/list">
           {projects?.map((data, i) => <Project data={data} key={i} />)}
         </ul>
+
+        <div className="mt-12">
+          <a
+            className="inline-flex items-center font-medium text-sm leading-tight text-gray-400 hover:text-gray-200 group"
+            aria-label="View Full Project Archive"
+            href="https://github.com/Devang47?tab=repositories"
+          >
+            <span className="border-b border-transparent pb-px transition group-hover:border-cyan-300 motion-reduce:transition-none">
+              View Full Project Archive
+            </span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </a>
+        </div>
       </div>
     </section>
   )
